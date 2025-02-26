024a8575e0fdee50e5b0e3caa2720f598d069879
J-Herika <138695574+J-Herika@users.noreply.github.com>
2025-02-26 10:54:39 +0300

    setup the clock functionality

--- src/Pomo.tsx
+++ src/Pomo.tsx
@@ -1,3 +1,33 @@
+import { useEffect, useState } from "react";
+
+enum State {
+  Pomodoro,
+  Clock,
+}
+
 export default function Pomo() {
-  return <></>;
+  const [currentState, setCurrentState] = useState<State>(State.Clock);
+  const [clock, setClock] = useState<Date>(new Date());
+
+  function currentStateHandler() {
+    currentState == State.Clock
+      ? setCurrentState(State.Pomodoro)
+      : setCurrentState(State.Clock);
+  }
+
+  function clockHandler() {
+    setClock(new Date());
+  }
+
+  useEffect(() => {
+    if (currentState == State.Clock) setInterval(clockHandler, 1000);
+  });
+  
+  return (
+    <p>
+      {currentState == State.Clock
+        ? `${clock.getHours()} : ${String(clock.getMinutes()).padStart(2, "0")} : ${String(clock.getSeconds()).padStart(2, "0")}`
+        : "Pomodoro"}
+    </p>
+  );
 }
