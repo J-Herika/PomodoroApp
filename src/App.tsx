fe792c0f1539ffb8b22131496854e6939821dab0
J-Herika <138695574+J-Herika@users.noreply.github.com>
2025-02-26 10:24:59 +0300

    add the header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import Header from "./header";
+import Pomo from "./Pomo";
+import Footer from "./Footer";
+
 function App() {
   return (
-    <>
-      <h1 className="text-red-400">Hello World</h1>
-    </>
+    <div className="flex h-screen flex-col items-center justify-between">
+      <Header />
+      <Pomo />
+      <Footer />
+    </div>
   );
 }
 
---
+++ src/Header.tsx
@@ -0,0 +1,5 @@
+export default function Header() {
+  const title: string = "MoKonai";
+
+  return <h1 className="text-5xl">{title}</h1>;
+}
